692b371254839eed5e0a37835841b97867164d1a
U-MOHIDUL\Zakia Abedin <moabedin00@gmail.com>
2021-07-09 22:09:48 -0400

    added cover image to home screen

--- movies/src/components/Home.js
+++ movies/src/components/Home.js
@@ -1,22 +1,36 @@
 import React, { useState } from "react";
 
+
+
 //Config
 import {POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL} from "../config";
 
 //Components 
-
+import CoverImage from "./CoverImage"
 
 //Hook
+import { useHomeFetch } from "../hooks/useHomeFetch";
+
 
 //Image
 import NoImage from "../images/no_image.jpg";
 
-const Home = () => {
-    const [state, setState] = useState();
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState();
 
-    return <div> Home Page </div>
+
+const Home = () => {
+    const {state, loading, error } = useHomeFetch();
+    console.log(state.results[0]);
+    return (
+        <>
+        {state.results[0] ? (
+          <CoverImage
+            image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+            title={state.results[0].original_title}
+            text={state.results[0].overview}
+          />
+        ) : null}
+      </>
+    );
 }
 
 export default Home;
