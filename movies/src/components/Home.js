8a36eeaaaa875e832c6b019012f72f9fab6223fe
U-MOHIDUL\Zakia Abedin <moabedin00@gmail.com>
2021-07-11 17:41:46 -0400

    added popular movies in a grid pattern

--- movies/src/components/Home.js
+++ movies/src/components/Home.js
@@ -7,6 +7,7 @@ import {POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL} from "../config";
 
 //Components 
 import CoverImage from "./CoverImage"
+import Grid from "./Grid"
 
 //Hook
 import { useHomeFetch } from "../hooks/useHomeFetch";
@@ -22,13 +23,21 @@ const Home = () => {
     console.log(state.results[0]);
     return (
         <>
-        {state.results[0] ? (
-          <CoverImage
-            image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
-            title={state.results[0].original_title}
-            text={state.results[0].overview}
-          />
-        ) : null}
+            {state.results[0] ? (
+            <CoverImage
+                image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+                title={state.results[0].original_title}
+                text={state.results[0].overview}
+            />
+            ) : null}
+            <Grid header= "Popular Movies">
+                {state.results.map(movies => (
+                    <div key={movies.id}> {movies.title} </div>   
+                    
+                ))}
+            </Grid>    
+      
+      
       </>
     );
 }
