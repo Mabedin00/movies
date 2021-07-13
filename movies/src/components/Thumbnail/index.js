47fd223bc925f6ac02624ab4c9072029b6f6c5fd
U-MOHIDUL\Zakia Abedin <moabedin00@gmail.com>
2021-07-13 17:20:41 -0400

    added routing

--- movies/src/components/Thumbnail/index.js
+++ movies/src/components/Thumbnail/index.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 //Styles
+import { Image } from "./Thumbnail.styles";
 
-import { Image } from "./Thumbnail.styles"
-
-const Thumbnail = ({ image, movieId, clickable}) => (
+const Thumbnail = ({ image, movieId, clickable }) => (
     <div>
-        <Image src={image} alt="movie-thumbnail" />
+        {clickable ? (
+            <Link to={`/${movieId}`}>
+                <Image src={image} alt="movie-thumbnail" />
+            </Link>
+        ) : (
+            <Image src={image} alt="movie-thumbnail" />
+        )}
     </div>
-)
+);
 
 export default Thumbnail;
